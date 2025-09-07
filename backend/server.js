5442978e0bcb0ec4f4b2a910cc6ad89523c6f7f7
maroua <marouadjili2@gmail.com>
2025-09-07 17:08:12 +0100

    fix: update backendddd

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'https://amira-mahdad-omega.vercel.app',
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
