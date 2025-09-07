fac065514cbec8d21dd889d13d206655ce2c5cf7
maroua <marouadjili2@gmail.com>
2025-09-07 17:21:58 +0100

    fix: update backendddd hhh

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: 'https://amira-mahdad-omega.vercel.app',
+  origin: 'https://amiramahdad.netlify.app/',
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
